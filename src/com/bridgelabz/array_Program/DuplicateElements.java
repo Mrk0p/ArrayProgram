a56562457426edbc904bac0723396af26bf80f2e
Sachin wele <115727568+Mrk0p@users.noreply.github.com>
2023-01-18 19:56:04 +0530

    Added : check duplicate elements

--- src/com/bridgelabz/array_Program/DuplicateElements.java
+++ src/com/bridgelabz/array_Program/DuplicateElements.java
@@ -8,7 +8,7 @@ public class DuplicateElements {
 
         System.out.println("Duplicate elements:- ");
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) { 
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j])
                     System.out.print(array[j] + " ");
